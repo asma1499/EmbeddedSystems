1b8a4355513ecaae66c3f479bbbaa906bea10bd2
asma1499 <asmaa.ettesh32@gmail.com>
2023-01-03 14:53:00 +0200

    First ES Project

---
+++ 01-DIO_Basics/main.c
@@ -0,0 +1,44 @@
+/*
+ * main.c
+ *
+ *  Created on: 2 Jan 2023
+ *      Author: asmaa
+ */
+#include <avr/io.h>
+#include <util/delay.h>
+
+int main (void)
+{
+	DDRA = 0b00001111; //PA0 -> Output == DDRA = 0X0f;
+	/*PORTA = 0b00000001; // PA0 -> HIGH(5V)
+	_delay_ms(1000);
+	PORTA = 0; //PA0->LOW
+	_delay_ms(1000);*/
+
+	while(1) //Super loop
+	{
+		/*PORTA = 0b00000001; // PA0 -> HIGH(5V)
+		_delay_ms(1000);
+		PORTA = 0; //PA0->LOW
+		_delay_ms(1000);*/
+		PORTA =0b00000001;
+		for (int i = 0 ; i < 4 ; i++)
+		{
+			_delay_ms(250);
+			PORTA <<= 1;
+
+			if ( PORTA == 0b00010000 ) //PORTA = 0b00001000
+			{
+				for(int x = 0; x<4; x++)
+				{
+					PORTA >>= 1;
+					_delay_ms(250);
+
+				}
+			}
+
+		}
+
+      }
+}
+
